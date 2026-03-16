d593ce736ee5bf8a06cc24d58d1d96121a9719b3
nemanja2424 <njakovlje@gmail.com>
2026-03-17 00:15:45 +0100

    admin debug

--- routes/admin.py
+++ routes/admin.py
@@ -17,25 +17,7 @@ admin_bp = Blueprint("admin", __name__)
 def hello():
     return jsonify({"message": "Zdravo ADMINE!"})
 
-@admin_bp.route('/token', methods=['POST'])
-def get_token():
-    data = request.json
-    if not data:
-        return jsonify({"error": "Nedostaje JSON u zahtevу"}), 400
-    
-    username = data.get("username")
-    password = data.get("password")
-    env_username = os.getenv("ADMIN_USERNAME")
-    env_password = os.getenv("ADMIN_PWD")
-    
-    if username == env_username and password == env_password:
-        # expires_delta=None znači da token NE ISTIČE
-        token = create_access_token(identity=username, expires_delta=None)
-        return jsonify(access_token=token), 200
-    return jsonify(msg="Bad credentials"), 401
-
-
-@admin_bp.route('/paket_limits', methods=['POST'])
+@admin_bp.route('/paket_limits', methods=['PATCH'])
 @jwt_required()
 def update_paket_limits():
     """
@@ -124,7 +106,7 @@ def update_paket_limits():
         }), 500
 
 
-@admin_bp.route('/ai_info', methods=['POST'])
+@admin_bp.route('/ai_info', methods=['PATCH'])
 @jwt_required()
 def update_ai_info():
     """
@@ -213,7 +195,7 @@ def update_ai_info():
         }), 500
 
 
-@admin_bp.route('/paket', methods=['POST'])
+@admin_bp.route('/paket', methods=['PATCH'])
 @jwt_required()
 def update_paket():
     """
